718fb0b3f149f752aa52ec327238d6b05feeed58
gid <d.giger.m@gmail.com>
2021-04-13 10:25:26 +0200

    t

--- start.py
+++ start.py
@@ -20,11 +20,6 @@ def showBPMN(file_path):
     process_tree = pm4py.discover_tree_inductive(log)
     bpmn_model = pm4py.convert_to_bpmn(process_tree)
     pm4py.view_bpmn(bpmn_model)
-
-def showPetri(file_path):
-    log = pm4py.read_xes(file_path)
-    petri_net, initial_marking, final_marking = pm4py.discover_petri_net_alpha(log)
-    pm4py.view_petri_net(petri_net, initial_marking, final_marking, format='png')
   
 if __name__ == "__main__":
     showPetri('E:/PM/running-example.xes')
