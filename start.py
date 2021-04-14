f93ef6ac6bdc822c7ccef43b18fdb02af2779f7b
gid <d.giger.m@gmail.com>
2021-04-14 14:27:09 +0200

    t

--- start.py
+++ start.py
@@ -36,3 +36,14 @@ def results():
 
 if __name__ == "__main__":
     app.run()	
+
+@app.route('/upload')
+def upload_file():
+   return render_template('upload.html')
+
+  @app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
