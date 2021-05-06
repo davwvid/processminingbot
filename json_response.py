a048fbaf0e079d6101964093940d9827e8d6550f
gid <d.giger.m@gmail.com>
2021-05-06 17:51:06 +0200

    Your Message

--- json_response.py
+++ json_response.py
@@ -8,3 +8,30 @@ def text_response(text):
       }
     ]
   }
+
+def fileupload_response():
+  return {
+    "richContent": [
+      [
+        {
+          "text": {
+            "text": [text]
+          }
+        },
+        {
+          "type": "button",
+          "icon": {
+            "type": "chevron_right",
+            "color": "#FF9800"
+          },
+          "text": "Go to file upload",
+          "link": "https://processminingbot.herokuapp.com/upload.html",
+          "event": {
+            "name": "",
+            "languageCode": "",
+            "parameters": {}
+          }
+        }
+      ]
+    ]
+  }
