816e431339cd57210ebc5bb9272cb8bb8ba3e934
gid <d.giger.m@gmail.com>
2021-05-06 20:06:53 +0200

    Your Message

--- start.py
+++ start.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     app.run(debug=True)	
 
 def mining():
-    log = pm4py.csv('uploads\running-example.csv')
+    log = pm4py.read_csv('uploads\running-example.csv')
     process_tree = pm4py.discover_tree_inductive(log)
     bpmn_model = pm4py.convert_to_bpmn(process_tree)
     pm4py.view_bpmn(bpmn_model)
